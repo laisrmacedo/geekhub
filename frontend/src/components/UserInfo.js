b4410cd350115090f79a523ee31dcbe7eb85fe77
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-09-27 18:55:52 +0200

    feat: user info component

---
+++ frontend/src/components/UserInfo.js
@@ -0,0 +1,89 @@
+// import { Footer } from "../components/Footer"
+import styled from "styled-components";
+// import axios from "axios";
+// import { BASE_URL } from "../App";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+// import { goToLoginPage } from "../router/coordinator";
+import { useEffect } from "react";
+
+import { InputForShortText } from "../GlobalStyle";
+
+const Content = styled.aside`
+  width: 25%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  gap: 20px;
+  padding-top: 20px;
+  /* border: 1px red solid; */
+
+  figure{
+    display: flex;
+    gap: 16px;
+    height: 70px;
+    width: 70px;
+    img{
+      border-radius: 10px;
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .user-id{
+    text-align: center;
+    h4{
+      font-size: 12px;
+      font-weight: 500;
+    }
+    p{
+      font-size: 10px;
+      color: #7E7E88;
+    }
+  }
+
+  .user-details{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    padding: 20px;
+    border-radius: 10px;
+    background: #fff;
+    font-size: 10px;
+    color: #7E7E88;
+    span{
+      width: 100%;
+    }
+  }
+  .borderTopBottom{
+    border-top: 1px solid #F0F0F0;
+    border-bottom: 1px solid #F0F0F0;
+    padding: 8px 0;
+  }
+`
+
+export const UserInfo = () => {
+  const navigate = useNavigate()
+
+  return (
+    <Content>
+      <figure>
+        <img src="https://img.freepik.com/fotos-gratis/mulher-bonita-com-cabelo-encaracolado-mantem-a-mao-no-pescoco-sorri-gentilmente-usa-um-macacao-casual-carmesim-olha-feliz-para-a-camera_273609-39135.jpg"/>
+      </figure>
+      <div className="user-id">
+        <h4 className="name">Nome da Pessoa</h4>
+        <p className="nickname">@pessoa</p>
+      </div>
+      <div className="user-details">
+        <span>pessoa@gmail.com</span>
+        <span className="borderTopBottom">30 posts</span>
+        <span>77 comments</span>
+      </div>
+    </Content>
+  )
+}
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,23 +1,40 @@
 import { Container } from '../components/Container';
 import { Header } from '../components/Header';
 import styled from "styled-components";
-import { heightHeader } from "../GlobalStyle";
+import { UserInfo } from '../components/UserInfo';
 
 const Content = styled.div`
   height: calc(100% - 80px);
-  width: 360px;
+  width: 100%;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   /* border: 1px red solid; */
+  section{
+    height:100%;
+    width: 50%;
+    border-left: 1px solid #F0F0F0;
+    border-right: 1px solid #F0F0F0;
+    padding: 8px 0;
+  }
+  aside{
+    height:100%;
+    width: 25%;
+
+  }
 `
 
 export const Dashboard = () => {
   return(
     <Container>
       <Header></Header>
-      <Content></Content>
+      <Content>
+        <UserInfo/>
+        <section>
+
+        </section>
+        <aside></aside>
+      </Content>
     </Container>
   )
 }
