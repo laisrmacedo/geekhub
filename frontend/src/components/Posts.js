ee291c6c465e085f0b653f2c93fa04cbc1bf4ee0
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-10-02 16:58:55 +0200

    refactor: code organization and modal

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -6,7 +6,7 @@ import styled from "styled-components";
 import { DiscussionCard } from "./DiscussionCard";
 import axios, { all } from "axios";
 import { BASE_URL } from "../App";
-import { GlobalContext } from "../context/GlobalContext";
+import { GlobalContext, getPosts } from "../context/GlobalContext";
 import ReactModal from 'react-modal';
 import "../modal.css"
 import { NewPost } from "./NewPost";
@@ -18,63 +18,51 @@ const Content = styled.div`
   flex-direction: column;
   justify-content: start;
   align-items: center;
-  gap: 20px;
+  gap: 10px;
   padding-top: 20px;
   
   .new-post{
     width: 100%;
-    height: 10%;
+    height: 40px;
     display: flex;
     gap: 4px;
     justify-content: center;
     align-items: center;
-    span{
+    div{
+      width: 100%;
       display: flex;
       align-items: center;
+      justify-content: space-between;
       border: 1px #E5E5E5 solid;
       border-radius: 10px;
-      width: calc(100% - 40px);
-      height: 40px;
-      padding-left: 16px;
+      height: 100%;
+      padding: 0 16px;
       color: #7E7E88;
       font-size: 12px;
       background: #f0f0f0;
     }
-    button{
-      width: 36px;
-      height: 36px;
-      font-size: 28px;
-      background: #4F709Caa;
-      /* background: #4F709C; */
-      color: #f0f0f0;
+    span{
+      font-size: 20px;
+      color: #4F709Caa;
     }
   }
   .container-posts{
     width: 100%;
-    height: 90%;
+    height: calc(100% - 40px);
     overflow-y: auto;
     overflow-x: hidden;
     /* display: flex; */
     gap: 8px;
     flex-direction: column;
     justify-content: start;
-    /* border: 1px red solid; */
   }
 `
 
-// export const ContainerPosts = styled.div`
-//   height: calc(100% - 195px);
-//   display: flex;
-//   flex-direction: column;
-//   align-items: start;
-//   width: 364px;
-//   gap: 10px;
-// `
 ReactModal.setAppElement('#root')
 
 export const Posts = () => {
   const navigate = useNavigate()
-  const { getPosts, allPosts } = useContext(GlobalContext)
+  const { allPosts, setAllPosts } = useContext(GlobalContext)
   const [content, setContent] = useState("")
 
   const onChangePost = (e) => {
@@ -95,7 +83,7 @@ export const Posts = () => {
     if(localStorage.getItem("token") === "" || allPosts === null){
       goToLogin(navigate)
     }else{
-      getPosts('/posts', headers)
+      getPosts('/posts', headers, setAllPosts)
     }
   }, [])
 
@@ -122,8 +110,7 @@ export const Posts = () => {
         <NewPost closeModal={handleCloseModal}/>
       </ReactModal>
       <div className="new-post" onClick={() => handleOpenModal()}>
-        <span>Add a new post</span>
-        <Btn>+</Btn>
+        <div>Add a new post<span>+</span></div>
       </div>
       <div className="container-posts">
       {allPosts.map((post) => {
