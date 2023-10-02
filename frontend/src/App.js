064d3ac33e2fb72e6aa3191626dccb13c04ac653
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-10-02 16:50:17 +0200

    refactor: code organization and reuse

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,19 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Router } from "./router/Router";
 import { GlobalStyle } from "./GlobalStyle";
 import { GlobalContext } from "./context/GlobalContext";
-import axios from "axios";
-
-export const BASE_URL = 'http://localhost:3003'
 
 function App() {
-  const [currentTheme, setCurrentTheme] = useState(false || JSON.parse(localStorage.getItem("theme")))
   const [allPosts, setAllPosts] = useState([])
 
-  const getPosts = async (path, headers) => {
-    try {
-      const response = await axios.get(BASE_URL + path, headers)
-      setAllPosts(response.data.sort((a,b)=> Date.parse(b.createdAt) - Date.parse(a.createdAt)))
-    } catch (error) {
-      console.log(error.response.data)
-      setAllPosts(null)
-    }
-  }
-
-  useEffect(()=>{
-    localStorage.setItem("theme", currentTheme)
-  },[currentTheme])
-
   const context = {
-    currentTheme,
-    setCurrentTheme,
-    getPosts,
-    allPosts
+    allPosts,
+    setAllPosts
     // mobileBreakPoint,
   }
   return (
     <GlobalContext.Provider value={context}>
-      <GlobalStyle theme={currentTheme}/>
+      <GlobalStyle/>
       <Router/>
     </GlobalContext.Provider>
   );
